7536f2296ed140274cb30911369570f2ead20562
Ramona <rare2400@student.miun.com>
2026-02-13 15:14:34 +0100

    first commit

---
+++ WorkoutTracker/Data/ApplicationDbContext.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace WorkoutTracker.Data;
+
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
+{
+}
