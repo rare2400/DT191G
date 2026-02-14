2cb18f6b74c5f65c21622770959100d6cada53ed
Ramona <rare2400@student.miun.com>
2026-02-14 11:19:07 +0100

    databaskontext och inkludering av alla modeller

--- WorkoutTracker/Data/ApplicationDbContext.cs
+++ WorkoutTracker/Data/ApplicationDbContext.cs
@@ -1,8 +1,20 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using WorkoutTracker.Models;
 
 namespace WorkoutTracker.Data;
 
-public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
+public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
 {
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<WorkoutModel> Workouts { get; set; } = null!;
+    public DbSet<WorkoutTypeModel> WorkoutTypes { get; set; } = null!;
+    public DbSet<ExerciseModel> Exercises { get; set; } = null!;
+    public DbSet<CategoryModel> Categories { get; set; } = null!;
+    public DbSet<WorkoutExerciseModel> WorkoutExercises { get; set; } = null!;
 }
+
