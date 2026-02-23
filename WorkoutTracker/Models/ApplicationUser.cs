79628bde9c04160817ea4c836cd9ff62b61b011f
Ramona <rare2400@student.miun.com>
2026-02-23 12:57:28 +0100

    lägger till förnamn och efternamn i ApplicationUser-modellen

--- WorkoutTracker/Models/ApplicationUser.cs
+++ WorkoutTracker/Models/ApplicationUser.cs
@@ -5,4 +5,13 @@ namespace WorkoutTracker.Models;
 
 public class ApplicationUser : IdentityUser
 {
+    [Required(ErrorMessage = "Ange ditt förnamn.")]
+    [StringLength(50, ErrorMessage = "Förnamnet får inte vara längre än 50 tecken.")]
+    [Display(Name = "Förnamn")]
+    public required string FirstName { get; set; }
+
+    [Required(ErrorMessage = "Ange ditt efternamn.")]
+    [StringLength(50, ErrorMessage = "Efternamnet får inte vara längre än 50 tecken.")]
+    [Display(Name = "Efternamn")]
+    public required string LastName { get; set; }
 }
