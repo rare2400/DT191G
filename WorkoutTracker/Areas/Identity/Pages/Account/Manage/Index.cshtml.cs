613053114393b792763e762f06fa51b729f0ac6f
Ramona Reinholdz <rare2400@student.miun.se>
2026-03-19 10:30:00 +0100

    Översätter engelsk text till svenska ochh lägger till klass på länkar för styling

--- WorkoutTracker/Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
+++ WorkoutTracker/Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
@@ -30,6 +30,7 @@ namespace WorkoutTracker.Areas.Identity.Pages.Account.Manage
         ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
         ///     directly from your code. This API may change or be removed in future releases.
         /// </summary>
+        [Display(Name = "E-post")]
         public string Username { get; set; }
 
         /// <summary>
